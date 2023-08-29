3adab87eb6181d4d90ddedbc8df28921c9577dc3
ManzoorAhmad1 <manzoor8132@gmail.com>
2023-08-29 06:00:57 -0700

    signUpForm

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
+import React, { useState } from "react"
 import Header from './FrontEnd/Header/Index';
 import SignIn from "./FrontEnd/LoginForm/Index";
+import SignUpForm from './FrontEnd/SignUp/Index';
 function App() {
+  const [showFrom, setShowForm] = useState(false)
+  const ShowSignUpForm = () => {
+    setShowForm(true)
+  }
   return (
     <div>
-      <Header />
-      <SignIn />
+      <Header showSignUp={ShowSignUpForm} />
+      {!showFrom ? <SignIn /> : <SignUpForm />}
     </div>
   );
 }
