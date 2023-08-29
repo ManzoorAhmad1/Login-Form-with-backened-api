5df7741aa5684b3e52e948cfbe7c3664a3d7ce8d
ManzoorAhmad1 <manzoor8132@gmail.com>
2023-08-29 04:51:05 -0700

    Header jsx

---
+++ src/FrontEnd/Header/Index.js
@@ -0,0 +1,10 @@
+import header from "./Index.module.css"
+
+const Header = () => {
+    return (
+        <div className={header.header}>
+            <h1><a href="/">Sign Up</a></h1>
+        </div>
+    )
+}
+export default Header;
